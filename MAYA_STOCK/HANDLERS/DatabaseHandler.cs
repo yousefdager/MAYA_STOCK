43a59d8df588a7d29a06fa960e3b093cb7e4dcac
ShiroKage_190 <mohammadosama15@gmail.com>
2016-12-15 22:23:12 -0800

    HANDLERS

---
+++ MAYA_STOCK/HANDLERS/DatabaseHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+using MIS_Project.Pages.Entities;
+
+namespace MIS_Project.Pages.Handlers
+{
+    public class DatabaseHandler
+    {
+        private static DatabaseHandler _instance;
+        public static DatabaseHandler Instance => _instance ?? (_instance = new DatabaseHandler());
+        public DataContext Con;
+        public void InitConnection()
+        {
+            var connString = Resources.LocalizedText.DBConnection;
+            var sqlCon= new SqlConnection(connString);
+            Con = new DataContext(sqlCon);
+        }
+        private DatabaseHandler()
+        {
+            if (Con == null) InitConnection();
+        }
+    }
+}
