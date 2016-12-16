ffec8051f31b75f39594bad97d6c0a2b32d4cd0d
mahmoudshkeeb <mahmoudshkeeb@yahoo.com>
2016-12-16 13:23:07 -0800

    MAHMOUD LINQ

---
+++ MAYA_STOCK/ENTITIES/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq ; ///////1
+using System.Data.Linq.Mapping ; //2
+
+namespace MAYA_STOCK.ENTITIES
+{
+
+    [Table (Name = "ADMIN_USERS")]
+    class Class1
+    {
+        [Column(Name = "USER_ID")]
+        public Int32  USER_ID { get; set;}
+        [Column(Name = "USER_NAME")]
+        public string user_name { get; set; }
+        [Column(Name = "ACTIVE_FLG")]
+        public Int32  active_flg { get; set; }
+        [Column(Name = "USER_TYPE")]
+        public Int32  user_type { get; set; }
+        [Column(Name = "PASSWORD")]
+        public string password { get; set; }
+
+
+    }
+}
--- MAYA_STOCK/Form1.cs
+++ MAYA_STOCK/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Linq;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MAYA_STOCK.ENTITIES;
 
 namespace MAYA_STOCK
 {
@@ -26,6 +28,19 @@ namespace MAYA_STOCK
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            /***************************************************************/
+            /*connection string */
+            DataContext A = new DataContext("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\maya\MAYA_STOCK\My_DB\STOCK_DB.mdf;Integrated Security=True");
+            var class1s = A.GetTable<Class1>();
+           /****************************************************/
+
+
+            var mdata = from data in class1s where data.USER_ID == 1 select data;
+            if (mdata.Any())
+            {
+               bool a= mdata.ToList()[0].password == "pass";
+            }
+
             MessageBox.Show("abu rafeeq kiss my ass");
         }
     }
